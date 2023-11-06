1217c2f76515acb2d477c2fec35f647d58925cee
khai <khaitruong1301@gmail.com>
2023-11-06 21:24:51 +0700

    buoi1_component_data_binding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom/client';
+import HomeLayout from './components/HomeLayout/HomeLayout';
+import Databinding from './DataBinding/Databinding';
+
 //Tạo root trên thẻ div#root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //JSX
 root.render(
     <div className="abc">
-      hello cybersoft
+      {/* <HomeLayout /> */}
+      <Databinding />
     </div>
 );
+
