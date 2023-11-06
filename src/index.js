ee6b0b3fca12cfa23a38ec2372f1319e8d6f8e38
khai <khaitruong1301@gmail.com>
2023-11-06 19:16:53 +0700

    buoi1_setup_react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//Tạo root trên thẻ div#root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//JSX
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className="abc">
+      hello cybersoft
+    </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
